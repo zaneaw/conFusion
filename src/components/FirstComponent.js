54c36d8aba81763f183322e59130b000e70cc3c8
Zane Wilson <wilsonzanea@gmail.com>
2022-01-10 18:29:17 -0500

    how to toggle the state of an element

--- src/components/FirstComponent.js
+++ src/components/FirstComponent.js
@@ -1,6 +1,7 @@
 // Change code below this line
+import React, { Component } from 'react';
 
-class MyComponent extends React.Component {
+class MyComponent extends Component {
   constructor(props) {
     super(props);
   }
@@ -15,3 +16,7 @@ class MyComponent extends React.Component {
 }
 
 ReactDOM.render(<MyComponent />, document.getElementById("challenge-node"));
+
+{/* Define a class MyComponent that extends React.Component. Its render method should return a div that contains an h1 tag with the text: My First React Component! in it. Use this text exactly, the case and punctuation matter. Make sure to call the constructor for your component, too.
+
+Render this component to the DOM using ReactDOM.render(). There is a div with id='challenge-node' available for you to use.*/}
