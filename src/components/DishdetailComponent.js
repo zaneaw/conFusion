4f00a46bc3b6b9652a165048b3c68858d6e6dc49
Zane Wilson <wilsonzanea@gmail.com>
2022-01-13 22:18:06 -0500

    react parameters and bootstrap/react breadcrumbs

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 const RenderDish = ({ dish }) => {
   if (dish != null) {
@@ -43,16 +52,33 @@ const RenderComments = ({ comments }) => {
       </div>
     );
   } else {
-    return <div></div>;
+    return <div />;
   }
 };
 
 const DishDetail = (props) => {
   if (props.dish != null) {
     return (
-      <div className="row">
-        <RenderDish dish={props.dish} />
-        <RenderComments comments={props.dish.comments} />
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/home">Home</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <RenderDish dish={props.dish} />
+          <RenderComments comments={props.comments} />
+        </div>
       </div>
     );
   } else {
